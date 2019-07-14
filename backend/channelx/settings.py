590fa44a1584e41371a08d8e292933a24311c1c5
Lurge <glngwenya@gmail.com>
2019-07-14 21:02:45 +0300

    Project setup

--- backend/channelx/settings.py
+++ backend/channelx/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chat',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,6 +71,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'channelx.wsgi.application'
 
+ASGI_APPLICATION = 'channelx.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
